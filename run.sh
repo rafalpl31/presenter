2aae68668c009616ad709ef1f41e142786da6d6b
Git Server Admin <admin@git-server>
2020-01-25 09:03:58 +0000

    Add initial repository

---
+++ run.sh
@@ -0,0 +1,5 @@
+# Please put a proper bash command that will build and run you application
+# For example if you build a Java application:
+# mvn clean package
+# java -jar target/your-application-1.0-SNAPSHOT.jar
+echo Building and running the application
