fdfde6dab85f98635eff05e893a10088544063ac
Rafal Skupien <rafal.skupien33@gmail.com>
2020-01-25 20:01:56 +0100

    add UT scenarios for fetchSunriseSunsetData

---
+++ app/src/client/Utils/serviceCalls/fetchSunriseSunsetData.test.js
@@ -0,0 +1,12 @@
+// We can discuss test cases for below during interview
+describe('fetchSunriseSunsetData', () => {
+
+    it('calls expected functions for positive test case', () => {
+
+    });
+
+    it('calls expected functions for negative test case', () => {
+
+    });
+
+});
